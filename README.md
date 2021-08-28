6c85348f1abb9221a2120232ea520729ba348680
Anshul Raheja <40492015+anshulraheja@users.noreply.github.com>
2021-08-28 22:31:24 +0530

    fix: validate input data

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# levelZero_markEleven
-Created with CodeSandbox
+# BdayLuckyorNot?
+
+### Description
+
+###### (levelZero_markEleven)
+
+The app checks if the user birthdate is lucky or not by checking if the user's birthdate is divisible by user's lucky number.
+
+### My learnings
+
+- useState hook
+- Data validation
+- modulus operator
+
+### Tech Stack
+
+React, CSS
+
+# Installation
+
+```bash
+git clone https://github.com/anshulraheja/levelZero_markEleven.git
+npm install
+```
--- src/components/Birthday.js
+++ src/components/Birthday.js
@@ -1,22 +1,38 @@
 import { useState } from "react";
-
+import { AiOutlineGithub, AiOutlineInstagram } from "react-icons/ai";
+import { FaDiscord, FaLinkedin } from "react-icons/fa";
+import { CgWebsite } from "react-icons/cg";
 export default function Birthday() {
   const [dob, setDOB] = useState();
   const [number, setNumber] = useState();
   const [output, setOutput] = useState();
 
   function checkLuckyBday() {
+    if (
+      dob === undefined ||
+      dob === "" ||
+      number === undefined ||
+      number === ""
+    ) {
+      setOutput("Please enter data in all the input fields");
+      return;
+    }
     let newDOB = dob.replaceAll("-", "");
     let sum = 0;
     for (let i = 0; i < newDOB.length; i++) {
       sum = sum + parseInt(newDOB[i]);
     }
-    if (parseInt(sum % number) === 0) setOutput("Lucky");
-    else setOutput("Unlucky");
+    if (parseInt(sum % number) === 0)
+      setOutput("Yay! Your birth date is lucky");
+    else setOutput("Sorry! Your birth date is unlucky");
   }
   return (
     <div>
-      <h1>is your bday lucky</h1>
+      <h1>Is your birth date lucky?</h1>
+      <h4> Check if your Birthday is lucky or not</h4>
+      <h6>No data is stored. We respect user priavcy</h6>
+      <label for="dob">Date of Birth</label>
+      <br />
       <input
         type="date"
         name="dob"
@@ -24,6 +40,9 @@ export default function Birthday() {
         value={dob}
         onChange={(e) => setDOB(e.target.value)}
       />
+      <br />
+      <label for="number">Enter your lucky number</label>
+      <br />
       <input
         type="number"
         name="number"
@@ -35,6 +54,31 @@ export default function Birthday() {
         check
       </button>
       <div>{output}</div>
+
+      <footer>
+        <ul class="footer-list">
+          <li>
+            <a class="links" href="/" target="_blank">
+              <AiOutlineGithub />
+            </a>
+          </li>
+          <li>
+            <a class="links" href="/" target="_blank">
+              <FaLinkedin />
+            </a>
+          </li>
+          <li>
+            <a class="links" href="/" target="_blank">
+              <FaDiscord />
+            </a>
+          </li>
+          <li>
+            <a class="links" href="/" target="_blank">
+              <CgWebsite />
+            </a>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 }
